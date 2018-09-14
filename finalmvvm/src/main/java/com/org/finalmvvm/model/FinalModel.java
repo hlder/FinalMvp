b95265a6016c3ace3c86bcb9278a3f8002bf1922
yuanfen7650 <591929693@qq.com>
2018-09-14 16:44:52 +0800

    初始化

---
+++ finalmvvm/src/main/java/com/org/finalmvvm/model/FinalModel.java
@@ -0,0 +1,7 @@
+package com.org.finalmvvm.model;
+
+/**
+ * 获取数据
+ */
+public interface FinalModel {
+}
---
+++ finalmvvm/src/main/java/com/org/finalmvvm/rxs/RxUtils.java
@@ -0,0 +1,31 @@
+package com.org.finalmvvm.rxs;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.Callable;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+public class RxUtils {
+
+    /**
+     * 执行类
+     */
+    public static void execute(Object Object, Method method, Object ... params){
+        execute(new RxCallable(Object,method,params));
+    }
+
+    private static void execute(Callable<ObservableSource<? extends Object>> callable){
+        Observable<Object> observable=Observable.defer(callable);
+        observable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new RxDisposableObserver());
+    }
+
+
+
+
+}
