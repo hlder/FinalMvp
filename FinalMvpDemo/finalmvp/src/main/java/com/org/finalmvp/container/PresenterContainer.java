a681539e343ade85159e0451b6c3a7619d6a8903
yuanfen7650 <591929693@qq.com>
2019-01-18 13:02:45 +0800

    a

--- FinalMvpDemo/finalmvp/src/main/java/com/org/finalmvp/container/PresenterContainer.java
+++ FinalMvpDemo/finalmvp/src/main/java/com/org/finalmvp/container/PresenterContainer.java
@@ -75,8 +75,8 @@ public class PresenterContainer {
                 String clsName=""+iter.nextElement();
 //                Log.d("dddd","clsName："+clsName);
                 try {
-                    Class cls=classLoader.loadClass(clsName);
-//                    Class cls=Class.forName(clsName);
+//                    Class cls=classLoader.loadClass(clsName);
+                    Class cls=Class.forName(clsName);
                     Annotation vm= cls.getAnnotation(MvpPresenter.class);
                     Annotation model= cls.getAnnotation(MvpModel.class);
                     if(vm!=null||model!=null){
